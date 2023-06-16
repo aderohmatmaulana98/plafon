e8c51cc2dad307a38c7a82b42e72fd774a09df7b
agusmuhamadgi <gaostulibad@gmail.com>
2023-06-16 11:28:09 +0700

    update api

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/login', [ApiAuthController::class, 'login']);
-Route::post('/password/forgot', [ApiAuthController::class, 'forgotPassword']);
-Route::post('/password/reset', [ApiAuthController::class, 'resetPassword']);
+
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/me', [ApiAuthController::class, 'me']);
@@ -29,12 +28,20 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
     //barang
     Route::get('/barang', [ApiAllController::class, 'barang']);
-    Route::get('/get_barang_by_id/{id}', [ApiAllController::class, 'get_barang_by_id']);
+    Route::post('/get_barang_by_id', [ApiAllController::class, 'get_barang_by_id']);
+
+    //distributor
+    Route::get('/distributor', [ApiAllController::class, 'distributor']);
+    Route::get('/count_manager', [ApiAllController::class, 'count_manager']);
+    Route::get('/penjab', [ApiAllController::class, 'penjab']);
+    Route::get('/users', [ApiAllController::class, 'users']);
+    Route::post('/tambah_distributor',[ApiAllController::class,'tambah_distributor']);
 
     //pemesanan
+    //pemesanan
     Route::get('/pemesanan',[ApiAllController::class, 'pemesanan']);
-    Route::get('/get_pemesanan_by_id/{id}',[ApiAllController::class, 'get_pemesanan_by_id']);
     Route::post('/tambah_pemesanan',[ApiAllController::class, 'tambah_pemesanan']);
     Route::delete('/delete_pemesanan',[ApiAllController::class, 'delete_pemesanan']);
+    Route::get('/get_pemesanan_by_id/{id}',[ApiAllController::class, 'get_pemesanan_by_id']);
     Route::post('/updateTransaksi', [ApiAllController::class, 'updateTransaksi']);
 });
